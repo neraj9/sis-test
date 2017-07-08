a2453287b09a86dc5085f947fddd7b29a2a16879
Neraj Sharma <neraj9@gmail.com>
2017-07-08 21:11:05 +0100

    test improvements

--- src/test/java/com/sis/TeamIntegrationTest.java
+++ src/test/java/com/sis/TeamIntegrationTest.java
@@ -2,8 +2,6 @@ package com.sis;
 
 import com.sis.model.Team;
 import com.sis.repository.TeamRepository;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,27 +31,6 @@ public class TeamIntegrationTest {
 
     @Autowired
     private TeamRepository teamRepository;
-
-    @Before
-    public void init(){
-        Team chelsea = new Team("Chelsea", "London", "Abramovich", 80000, "Premier League", 18, "13-06-1970");
-        Team manchesterCity = new Team("ManchesterCity", "Manchester", "Qatar Airways", 72000, "Premier League-2015", 21, "13-06-1997");
-
-        Team manUnited = new Team("ManchesterUnited", "Manchester", "ABC", 100000, "Premier League-2013", 18, "23-06-1950");
-
-        teamRepository.save(chelsea);
-        teamRepository.save(manchesterCity);
-        teamRepository.save(manUnited);
-    }
-
-    @After
-    public void destroy(){
-        teamRepository.delete("Chelsea");
-        teamRepository.delete("ManchesterCity");
-        teamRepository.delete("ManchesterUnited");
-    }
-
-
 
     @Test
     public void getTeamsShouldReturnTeams() throws Exception {
